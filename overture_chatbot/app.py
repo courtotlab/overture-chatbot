32131a522dcec56eaa9c14ae49b5577ae1968576
Ryan Seeto <rseeto@oicr.on.ca>
2024-09-12 12:26:18 -0400

    feat: add chainlit on_message to app

--- overture_chatbot/app.py
+++ overture_chatbot/app.py
@@ -1,7 +1,22 @@
 import chainlit as cl
+from langchain.schema.runnable.config import RunnableConfig
 
 @cl.on_chat_start
 async def on_chat_start():
     await cl.Message(content="Welcome to the Overture Chatbot!").send()
     langchain_chain = pass
     cl.user_session.set("runnable", langchain_chain)
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")
+
+    msg = cl.Message(content="")
+
+    for chunk in await cl.make_async(runnable.stream)(
+        {"query": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
