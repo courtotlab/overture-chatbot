1401c9d565222f13187bbb8c3857fb6792025cfc
Ryan Seeto <rseeto@oicr.on.ca>
2024-10-25 14:43:45 -0400

    build: expose port and run chainlit

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,5 @@ RUN pip install -r requirements.txt
 # grant permission
 RUN chmod +x run.sh
 
+EXPOSE 5000
 CMD ["./run.sh"]
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 python3 initialize_db/main.py
+chainlit run overture_chatbot/app.py --host=0.0.0.0 --port=5000 --headless
